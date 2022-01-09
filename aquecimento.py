ed16ef1f500bddbb0e301fa6f9709ebe922608bb
William Menghi Hirano <whirano4@gmail.com>
2022-01-09 14:42:11 -0300

    Melhorias na organização

--- aquecimento.py
+++ aquecimento.py
@@ -1,98 +1,130 @@
-nome = "Caio"
+# Tipos de Dados
 
+################
 # Saída de Dados
-# Print data type
+nome = "William"
+# Print Tipo de Dado
 print(type(nome))
-
+# Print Tamanho do Dado
+print(len(nome))
 # Concatenação de String
 print("Olá" + nome + ", tudo bem?")
-
 # Print Format de String
 print(f"Olá {nome}, tudo bem?")
+###############################
 
+##################
 # Entrada de Dados
 nome = input("Digite seu nome: ")
-print(f"Olá {nome}, sua idade é {idade}")
+idade = input("Digite sua idade: ")
+print(f"Olá {nome}, sua idade é {idade}.")
+# Exemplo:
+# Soma com Cast Int
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+print("A soma do primeiro número com o segundo é:" + n1 + n2)
+#############################################################
 
-# Soma e cast int
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
-print(n1 + n2)
-
+########################
 # Operadores Aritméticos
-# Soma +
-# Divisão /
-# Divisão por inteiro, ou sem casas decimais //
-# Exponenciação **
-# Resto da Divisão ou Módulo %
-# Raíz quadrada importar math e usar sqrt ou elevar a 1/2
+n1 = 1
+n2 = 2
+# Soma 
+print(n1 + n2)
+# Divisão 
+print(n1 / n2)
+# Divisão por inteiro ou sem casas decimais 
+print(n1 // n2)
+# Resto da Divisão ou Módulo 
+print(n1 % n2)
+# Exponenciação 
+print(n1 ** n2)
+# Raíz Quadrada 
+import math
+n1 = 1
+print(math.sqrt(n1))
+print(n1 ** 0.5)
+################
 
+########################
 # Operadores Relacionais
-# Atribuição =
-# Comparação ==
-# Menor <
-# Maior >
-# Menor e igual <=
-# Maior e igual >=
+n1 = 1
+n2 = 2
+# Atribuição 
+n1 = 1
+# Comparação 
+n1 == n2
+n1 != n2
+# Maior
+n1 > n2
+# Maior e Igual 
+n1 >= n2
+# Menor
+n1 < n2
+# Menor e Igual
+n1 <= n2
+########
 
+####################
 # Operadores Lógicos
-if 1 == 2:
-    print('if')
-elif 2 == 3:
-    print('elif')
+n1 = 1
+n2 = 2
+n3 = 3
+if n1 == n2:
+    print('primeiro caso')
+elif n1 == n3:
+    print('segundo caso')
 else:
-    print('else')
-
-nota1 = float(input('Digite uma nota: '))
-nota2 = float(input('Digite outra nota: '))
+    print('outros casos')
+# Exemplo:
+# Média com Cast Float
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
 if media >= 6:
-    print('APROVADO')
+    print('Aprovado')
 elif media >= 4 and media < 6:
-    print('RECUPERAÇÃO')
+    print('Recuperação')
 elif media < 4:
-    print('REPROVADO')
-
+    print('Reprovado')
 # Match Case, substituto do Switch Case no Python
+x = 'a'
+match x:
+    case 'a':
+        print('Caso 1')
+    case 'b':
+        print('Caso 2')
+#######################
 
-
-def f(x):
-    match x:
-        case 'a':
-            return 1
-        case 'b':
-            return 2
-        case _:
-            return 0
-
-
+#################
+# Laços Temporais
 # Laço While
+import time
 x = 0
 while x < 100:
     print(x)
-    continue  # Volta pro topo do loop e ignora as próximas linhas
     x += 1  # Incrementa um a x
-    sleep(0.5)  # Esperar meio segundo
-
-# Print tabuada do 5 inteira
+    time.sleep(0.5)  # Esperar meio segundo
+    continue  # Volta pro topo do loop e ignora as próximas linhas
+# Exemplo:
+# Tabuada do 5 
 x = 0
 while x < 10:
-    print(f"5 * {x} = {x * 5}")
+    print(f"5 x {x} = {x * 5}")
     x += 1
-
+# Laço For 
 # Começa no 2, vai até 10, com step de 2 em 2
 for i in range(2, 10, 2):
     print(i)
-
-for i in range(0, 3):
-    for j in range(0, 3):
-        print(f"{i} x {j} = {i * j}")
-
+# Exemplo:
+# Todas as tabuadas
 for i in range(1, 11):
-    print(f'==========[TABUADA DO {i}===========')
+    print(f'==========[TABUADA DO {i}==========')
     for j in range(1, 11):
         print(f"{i} x {j} = {i * j}")
+#####################################
 
+########
 # Listas
 notas = [6, 7, 8, 2, 9, 3]
 #        0  1  2  3  4  5
@@ -100,38 +132,39 @@ notas = [6, 7, 8, 2, 9, 3]
 notas.append(8)
 # Remove último da lista
 notas.pop()
-# Print tamanho
-print(len(notas))
-
-lista = []
+# Exemplo
+# Manipulando notas em uma lista de notas
+lista_notas = []
 while True:
     nota = int(input('Digite uma nota ou -1 para sair: '))
     if nota == -1:
         break
     else:
-        lista.append(nota)
-print(lista)
-
+        lista_notas.append(nota)
+print('Todas as notas: ' + lista_notas)
 soma = 0
-for nota in lista:
+for nota in lista_notas:
     soma += nota
-print("Soma: " + nota)
-print("Média: " + nota / len(lista))
+print('Soma: ' + nota)
+print('Média: ' + nota / len(lista_notas))
+##########################################
 
+############
 # Dicionário
 pessoa = {'nome': 'Caio Sampaio',
           'idade': 21,
           'cidade': 'Franca'}
-
 print(pessoa['cidade'])
+#######################
 
+#########
 # Funções
 def main():
     primeiraFuncao()
     segundaFuncao()
-
 def primeiraFuncao():
-    
+    pass
 def segundaFuncao():
-
+    pass
 main()
+######
